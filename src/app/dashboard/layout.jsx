98cea03c74dfb73b0ffc27ee30270ab7ba9d50bb
Nishit Jain <nishitj077@gmail.com>
2024-10-16 14:40:41 +0530

    improve user status in dashboard navbar

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -11,9 +12,12 @@ import ModalOpenButton from "./_components/ModalOpenButton";
 import SearchForm from "./_components/SearchForm";
 import BookmarkForm from "./_components/BookmarkForm";
 
-import { signOut } from "@utils/auth";
+import { auth, signOut } from "@utils/auth";
 
-export default function DashboardLayout({ children }) {
+export default async function DashboardLayout({ children }) {
+    const session = await auth();
+    const user = session.user;
+
     return (
         <div className="px-6 md:px-16 py-6 flex flex-col gap-6 relative">
             <nav className="navbar bg-base-100 glass rounded-box shadow-lg sticky top-6">
@@ -62,19 +66,65 @@ export default function DashboardLayout({ children }) {
                         </form>
                     </dialog>
 
-                    <form
-                        action={async () => {
-                            "use server";
-                            await signOut({ redirectTo: "/" });
-                        }}
-                    >
-                        <button type="submit" className="btn btn-primary">
-                            <FontAwesomeIcon icon={faArrowRightToBracket} />
-                            <span className="hidden sm:inline-block">
-                                Logout
-                            </span>
-                        </button>
-                    </form>
+                    <div className="dropdown dropdown-end">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className={`btn btn-ghost btn-circle avatar ${!user.image && "placeholder"}`}
+                        >
+                            <div
+                                className={`w-10 h-10 rounded-full relative ${!user.image && "bg-neutral text-neutral-content"}`}
+                            >
+                                {user.image ? (
+                                    <Image
+                                        alt={user.name}
+                                        src={user.image}
+                                        fill
+                                        priority
+                                        quality={100}
+                                    />
+                                ) : (
+                                    <span className="text-3xl-">
+                                        {user.name
+                                            .split(" ")
+                                            .map((s, i, arr) =>
+                                                i === 0 || i === arr.length - 1
+                                                    ? s[0]
+                                                    : "",
+                                            )
+                                            .join("")}
+                                    </span>
+                                )}
+                            </div>
+                        </div>
+
+                        <div
+                            tabIndex={0}
+                            className="dropdown-content card card-compact bg-base-100 z-[1] mt-3 min-w-52 shadow"
+                        >
+                            <div className="card-body">
+                                <span className="font-bold">{user.name}</span>
+                                <span>{user.email}</span>
+                                <form
+                                    action={async () => {
+                                        "use server";
+                                        await signOut({ redirectTo: "/" });
+                                    }}
+                                    className="card-actions mt-2"
+                                >
+                                    <button
+                                        type="submit"
+                                        className="btn btn-primary w-full"
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faArrowRightToBracket}
+                                        />
+                                        <span>Logout</span>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </nav>
 
